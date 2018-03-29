a4e529d385a3c2c9c658f131809a9b1a9f693bc0
Egor <egor.krutoychel.komp@mail.ru>
2018-03-29 12:54:21 +0300

    added service tests, update first task

---
+++ src/test/java/com/epam/makedon/agency/service/impl/UserServiceTest.java
@@ -0,0 +1,36 @@
+package com.epam.makedon.agency.service.impl;
+
+import com.epam.makedon.agency.service.Service;
+import com.epam.makedon.agency.service.ServiceException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserServiceTest {
+    private static Service service;
+
+    @Before
+    public void init() {
+        service = new UserServiceImpl();
+    }
+
+    @After
+    public void destroy() {
+        service = null;
+    }
+
+    @Test(expected = ServiceException.class)
+    public void exceptionAddTest() {
+        service.add(null);
+    }
+
+    @Test(expected = ServiceException.class)
+    public void exceptionRemoveTest() {
+        service.remove(null);
+    }
+
+    @Test(expected = ServiceException.class)
+    public void exceptionUpdateTest() {
+        service.update(null);
+    }
+}
