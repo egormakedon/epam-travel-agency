e83808c0857ea9c98776ddd4adb25d88a45ce558
Egor <egor.krutoychel.komp@mail.ru>
2018-05-27 14:24:44 +0300

    refactored CountryService: added annotations and profile support, added javadoc

--- src/main/java/com/epam/makedon/agency/service/impl/CountryServiceImpl.java
+++ src/main/java/com/epam/makedon/agency/service/impl/CountryServiceImpl.java
@@ -8,16 +8,22 @@ import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 /**
+ * Class CountryServiceImpl implements CountryService.
+ *
  * @author Yahor Makedon
  * @see com.epam.makedon.agency.service
  * @since version 1.0
  */
+@Service
+@Profile("service")
 public class CountryServiceImpl implements CountryService {
     private static final Logger LOGGER = LoggerFactory.getLogger(CountryServiceImpl.class);
 
@@ -28,8 +34,11 @@ public class CountryServiceImpl implements CountryService {
     public CountryServiceImpl() {}
 
     /**
-     * @param country
-     * @return
+     * Country add method. Supported with @Transactional (Isolation.REPEATABLE_READ, Propagation.REQUIRED)
+     *
+     * @param country object, which be adding to repository
+     * @return boolean, result of adding
+     * @throws ServiceException cause param is null or exception of adding
      */
     @Transactional(isolation = Isolation.REPEATABLE_READ)
     @Override
@@ -47,6 +56,13 @@ public class CountryServiceImpl implements CountryService {
         }
     }
 
+    /**
+     * Country get method. Supported with @Transactional (Isolation.READ_COMMITTED, Propagation.REQUIRED)
+     *
+     * @param id to identify and find country in repository
+     * @return country object, wrapped in Optional, cause null
+     * @throws ServiceException cause exception of getting
+     */
     @Transactional(isolation = Isolation.READ_COMMITTED)
     @Override
     public Optional<Country> get(long id) {
@@ -58,23 +74,49 @@ public class CountryServiceImpl implements CountryService {
         }
     }
 
+    /**
+     * Country remove method. Supported with @Transactional (Isolation.REPEATABLE_READ, Propagation.REQUIRED)
+     *
+     * @param country object, which be removing from repository
+     * @return boolean result of removing
+     * @throws ServiceException cause param is null or exception of removing
+     */
     @Transactional(isolation = Isolation.REPEATABLE_READ)
     @Override
     public boolean remove(Country country) {
         if (country == null) {
-            LOGGER.error("argument is null");
-            throw new ServiceException("argument is null");
+            LOGGER.error("country argument in remove method is null");
+            throw new ServiceException("country argument in remove method is null");
         }
-        return countryRepository.remove(country);
+
+        try {
+            return countryRepository.remove(country);
+        } catch (Exception e) {
+            LOGGER.error("", e);
+            throw new ServiceException(e);
+        }
     }
 
+    /**
+     * Country update method. Supported with @Transactional (Isolation.READ_UNCOMMITTED, Propagation.REQUIRED)
+     *
+     * @param country object, which be updating in repository
+     * @return country object, wrapped in Optional, cause null
+     * @throws ServiceException cause param is null or exception of updating
+     */
     @Transactional(isolation = Isolation.READ_UNCOMMITTED)
     @Override
     public Optional<Country> update(Country country) {
         if (country == null) {
-            LOGGER.error("argument is null");
-            throw new ServiceException("argument is null");
+            LOGGER.error("country argument in update method is null");
+            throw new ServiceException("country argument in update method is null");
         }
-        return countryRepository.update(country);
+
+        try {
+            return countryRepository.update(country);
+        } catch (Exception e) {
+            LOGGER.error("", e);
+            throw new ServiceException(e);
+        }
     }
 }
