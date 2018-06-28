ed29ca50dfa1ab31ae22e6a04878971d5e362573
Egor <egor.krutoychel.komp@mail.ru>
2018-06-28 09:39:36 +0300

    added MongodbConfiguration

---
+++ agency-domain/src/main/java/com/epam/makedon/agency/agencydomain/config/MongodbConfiguration.java
@@ -0,0 +1,54 @@
+package com.epam.makedon.agency.agencydomain.config;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableAspectJAutoProxy
+@ComponentScan("com.epam.makedon.agency.agencydomain.service")
+@EnableMongoRepositories("com.epam.makedon.agency.agencydomain.repository.mongodbimpl")
+@PropertySource("classpath:/property/mongodb.properties")
+public class MongodbConfiguration {
+
+    @Value("${mongodb.host}")
+    private String host;
+
+    @Value("${mongodb.database.name}")
+    private String databaseName;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public Mongo mongo() {
+        return new MongoClient(host);
+    }
+
+    @Bean
+    public MongoDbFactory mongoDbFactory() {
+        MongoClient mongoClient = (MongoClient) mongo();
+        return new SimpleMongoDbFactory(mongoClient, databaseName);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoDbFactory());
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcessor() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
