02dbf26ab92ad753d95140856c7174dde8ee3fe8
Egor <egor.krutoychel.komp@mail.ru>
2018-07-04 00:54:08 +0300

    added security service

--- agency-web/src/main/java/com/epam/makedon/agency/agencyweb/security/SecurityService.java
+++ agency-web/src/main/java/com/epam/makedon/agency/agencyweb/security/SecurityService.java
@@ -7,4 +7,7 @@ package com.epam.makedon.agency.agencyweb.security;
  * @version 1.0
  */
 public interface SecurityService {
+
+    String findLoggedInUsername();
+    void autoLogin(String username, String password);
 }
--- agency-web/src/main/java/com/epam/makedon/agency/agencyweb/security/SecurityServiceImpl.java
+++ agency-web/src/main/java/com/epam/makedon/agency/agencyweb/security/SecurityServiceImpl.java
@@ -1,10 +1,46 @@
 package com.epam.makedon.agency.agencyweb.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
 /**
  * Implementation of {@link SecurityService} interface
  *
  * @author Yahor Makedon
  * @version 1.0
  */
+@Service
 public class SecurityServiceImpl implements SecurityService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public String findLoggedInUsername() {
+        Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();
+        if (userDetails instanceof UserDetails) {
+            return ((UserDetails)userDetails).getUsername();
+        }
+        return null;
+    }
+
+    @Override
+    public void autoLogin(String username, String password) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+        authenticationManager.authenticate(authenticationToken);
+
+        if (authenticationToken.isAuthenticated()) {
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+    }
 }
