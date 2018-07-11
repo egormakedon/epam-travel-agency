70a9a8e447a55f1d134fc7f08ea3026530916a91
Egor <egor.krutoychel.komp@mail.ru>
2018-07-11 10:58:01 +0300

    refactored spring security config

--- agency-web/src/main/java/com/epam/makedon/agency/agencyweb/config/SecurityConfiguration.java
+++ agency-web/src/main/java/com/epam/makedon/agency/agencyweb/config/SecurityConfiguration.java
@@ -42,63 +42,31 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // включаем защиту от CSRF атак
         http.csrf()
                 .disable()
-                // указываем правила запросов
-                // по которым будет определятся доступ к ресурсам и остальным данным
                 .authorizeRequests()
-                .antMatchers("/resources/**", "/**").permitAll()
-                .anyRequest().permitAll()
-                .and();
-
-        http.formLogin()
-                // указываем страницу с формой логина
+                .antMatchers( "/", "/resources/**",
+                        "/public/**").permitAll()
+                .antMatchers("/admin/**").hasAuthority("ADMIN")
+                .anyRequest().fullyAuthenticated()
+                .antMatchers("/user/**").hasAuthority("USER")
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin()
                 .loginPage("/login")
-                // указываем action с формы логина
                 .loginProcessingUrl("/j_spring_security_check")
-                // указываем URL при неудачном логине
                 .failureUrl("/login?error")
-                // Указываем параметры логина и пароля с формы логина
-                .usernameParameter("j_username")
-                .passwordParameter("j_password")
-                // даем доступ к форме логина всем
-                .permitAll();
-
-        http.logout()
-                // разрешаем делать логаут всем
+                .usernameParameter("login")
+                .passwordParameter("password")
                 .permitAll()
-                // указываем URL логаута
+                .and()
+                .logout()
                 .logoutUrl("/logout")
-                // указываем URL при удачном логауте
+                .deleteCookies("remember-me")
                 .logoutSuccessUrl("/login?logout")
-                // делаем не валидной текущую сессию
-                .invalidateHttpSession(true);
-
+                .invalidateHttpSession(true)
+                .permitAll()
+                .and()
+                .rememberMe();
     }
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/", "/public/**",
-//                        "/resources/js/**", "/resources/css/**", "/resources/img/**").permitAll()
-//                .antMatchers("/admin/**").hasAuthority("ADMIN")
-//                .anyRequest().fullyAuthenticated()
-//                .antMatchers("/client/**").hasAuthority("CLIENT")
-//                .anyRequest().fullyAuthenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .failureUrl("/login?error")
-//                .usernameParameter("login")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .logoutUrl("/logout")
-//                .deleteCookies("remember-me")
-//                .logoutSuccessUrl("/")
-//                .permitAll()
-//                .and()
-//                .rememberMe();
-//    }
 }
