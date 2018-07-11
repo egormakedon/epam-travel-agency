4fc04abf97e1a99e936589aad85acf90afa1288f
Egor <egor.krutoychel.komp@mail.ru>
2018-07-11 11:57:46 +0300

    added MessageSecurityWebApplicationInitializer class

---
+++ agency-web/src/main/java/com/epam/makedon/agency/agencyweb/config/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,13 @@
+package com.epam.makedon.agency.agencyweb.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * MessageSecurityWebApplicationInitializer class,
+ * extends {@link AbstractSecurityWebApplicationInitializer} class.
+ *
+ * @author Yahor Makedon
+ * @version 1.0
+ */
+
+public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {}
